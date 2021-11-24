69c89fdafc2d2d251f39aba8dc2c8886483ec625
TonyPurple <tony.vanoni@outlook.com>
2021-11-24 11:41:42 -0800

    functioning properly, may make changes to personalize data

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,17 +2,52 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
 };
+
+function update(req, res) {
+    req.body.done = req.body.done === 'on';
+    Skill.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+    res.render('skills/edit', {
+        skill: Skill.getOne(req.params.id)
+    });
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function create(req, res) {
+    console.log(req.body);
+    req.body.done = false;
+    Skill.create(req.body);
+    // Do a redirect anytime data is changed
+    res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+    res.render('skills/new')
+}
 
 function index(req, res) {
     res.render('skills/index', {
-        skills: Skill.getAll()
+        skills: Skill.getAll(),
     });
 }
 
 function show(req, res) {
     res.render('skills/show', {
-        skill: Skill.getOne(req.params.id)
+        skill: Skill.getOne(req.params.id),
+        // skillNum: Skill.getAll().findIndex(skill => skill.id === parseInt(req.params.id)) + 1
     });
 }
--- models/skill.js
+++ models/skill.js
@@ -12,8 +12,28 @@ const skills = [
 
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
 };
+
+function update(id, skill) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    skill.id = parseInt(id);
+    skills.splice(idx, 1, skill);
+}
+
+function deleteOne(id) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    skills.splice(idx, 1)
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
+}
 
 function getAll() {
     return skills;
